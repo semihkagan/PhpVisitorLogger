cf0d36af405a86d99e807be4d35c787232d72624
semihkagan <97614413+semihkagan@users.noreply.github.com>
2024-07-18 23:29:05 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// Kullanıcının IP adresini al
+function getUserIpAddr() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        // IP adresi proxy sunucusu tarafından gönderilmişse
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        // IP adresi yük dengeleyici tarafından gönderilmişse
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        // Doğrudan erişim varsa
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+// Kullanıcının IP adresini al
+$user_ip = getUserIpAddr();
+
+// Zaman damgası ile birlikte IP adresini kaydet
+$log_entry = date('Y-m-d H:i:s') . " - " . $user_ip . PHP_EOL;
+
+// visitors.log dosyasına kaydet
+file_put_contents('visitors.log', $log_entry, FILE_APPEND);
+
+// echo "IP adresi kaydedildi.";
+?>
